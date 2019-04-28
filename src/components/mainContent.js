a3ae972422abf84486b306fe19741554f22eabb2
orozot <renzhitong12@outlook.com>
2019-04-28 18:35:13 +0800

    code init

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import Editor from './editor/editor'
 
 class MainContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editVal:""
+        }
+    }
+
+    onEditorValChange = (newVal) => {
+
+    }
+
     render(){
         return(
             <main className="content-main">
-                <Editor/>
+                <Editor onEditorValChange={this.onEditorValChange}/>
             </main>
         )
     }
